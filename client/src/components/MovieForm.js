2ebd0f97352ee21a58f5dfdf42cc563fb492c659
Kevin Diesenberg <kdiesenb@gmail.com>
2023-01-13 14:30:56 -0500

    async fetch cleeeean

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
-  const [errors, setErrors] = useState([])
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -15,25 +15,45 @@ function MovieForm() {
     female_director: false,
   });
 
-  function handleSubmit(e) {
+  // make the function async to enable the await keyword
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    // fetch returns a Promise, we must await it
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => {
-        if (response.ok) {
-          response.json().then((newMovie) => console.log(newMovie))
-        }
-        else {
-          response.json().then((errorData) => setErrors(errorData.errors))
-        }
-      })
-
+    });
+    // response.json() returns a Promise, we must await it
+    const data = await response.json();
+    if (response.ok) {
+      console.log("Movie created:", data);
+    } else {
+      setErrors(data.errors);
+    }
   }
+
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   fetch("/movies", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(formData),
+  //   })
+  //     .then((response) => {
+  //       if (response.ok) {
+  //         response.json().then((newMovie) => console.log(newMovie))
+  //       }
+  //       else {
+  //         response.json().then((errorData) => setErrors(errorData.errors))
+  //       }
+  //     })
+
+  // }
 
   function handleChange(e) {
     const value =
@@ -133,7 +153,7 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
-        
+
         {errors.length > 0 && (
           <ul style={{ color: "red" }}>
             {errors.map((error) => (
